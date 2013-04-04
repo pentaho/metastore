b69c946c61ccfb9ec349dba84cacdbad4309c67a
Matt Casters <mcasters@pentaho.org>
2013-04-04 17:34:16 +0200

    Pushing default element type constants into the metastore

--- src/org/pentaho/metastore/util/PentahoDefaults.java
+++ src/org/pentaho/metastore/util/PentahoDefaults.java
@@ -3,5 +3,11 @@ package org.pentaho.metastore.util;
 public class PentahoDefaults {
   
   public static final String NAMESPACE = "pentaho";
+ 
+  public static final String DATABASE_CONNECTION_ELEMENT_TYPE_NAME = "Database connection";
+  public static final String DATABASE_CONNECTION_ELEMENT_TYPE_DESCRIPTION = "This is the official central database connection metadata";
+  
+  public static final String KETTLE_DATA_SERVICE_ELEMENT_TYPE_NAME = "Kettle Data Service";
+  public static final String KETTLE_DATA_SERVICE_ELEMENT_TYPE_DESCRIPTION = "The elements describing data services based upon transformation output";
   
 }
