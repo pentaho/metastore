473a35323c7140e748f6d8c638cea11aa89c4b4d
bkemper <bkemper@pentaho.com>
2013-10-12 16:18:46 -0400

    Formatting, checkstyle pass 1

--- src/org/pentaho/metastore/stores/xml/XmlMetaStoreAttribute.java
+++ src/org/pentaho/metastore/stores/xml/XmlMetaStoreAttribute.java
@@ -1,19 +1,19 @@
 /*!
-* This program is free software; you can redistribute it and/or modify it under the
-* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
-* Foundation.
-*
-* You should have received a copy of the GNU Lesser General Public License along with this
-* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
-* or from the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See the GNU Lesser General Public License for more details.
-*
-* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
-*/
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ */
 
 package org.pentaho.metastore.stores.xml;
 
@@ -33,66 +33,67 @@ import org.w3c.dom.NodeList;
 public class XmlMetaStoreAttribute implements IMetaStoreAttribute {
 
   public static final String XML_TAG = "attribute";
-  
+
   protected String id;
   protected Object value;
 
   protected Map<String, IMetaStoreAttribute> children;
-  
+
   protected String filename;
-  
+
   public XmlMetaStoreAttribute() {
     children = new HashMap<String, IMetaStoreAttribute>();
     this.id = null;
     this.value = null;
   }
-  
-  public XmlMetaStoreAttribute(String id, Object value) {
+
+  public XmlMetaStoreAttribute( String id, Object value ) {
     this();
     this.id = id;
     this.value = value;
   }
-  
+
   /**
    * Duplicate the element data into this structure.
+   * 
    * @param element
    */
-  public XmlMetaStoreAttribute(IMetaStoreAttribute element) {
+  public XmlMetaStoreAttribute( IMetaStoreAttribute element ) {
     this();
     id = element.getId();
     value = element.getValue();
-    for (IMetaStoreAttribute childElement : element.getChildren()) {
-      addChild(new XmlMetaStoreAttribute(childElement));
+    for ( IMetaStoreAttribute childElement : element.getChildren() ) {
+      addChild( new XmlMetaStoreAttribute( childElement ) );
     }
   }
 
-  protected void loadAttribute(Node attributeNode) {
+  protected void loadAttribute( Node attributeNode ) {
     NodeList elementNodes = attributeNode.getChildNodes();
-    for (int e=0;e<elementNodes.getLength();e++) {
-      Node elementNode = elementNodes.item(e);
-      if ("id".equals(elementNode.getNodeName())) {
-        id = XmlUtil.getNodeValue(elementNode);
-      } else if ("value".equals(elementNode.getNodeName())) {
-        value = XmlUtil.getNodeValue(elementNode);
-      } else if ("type".equals(elementNode.getNodeName())) {
-        String type = XmlUtil.getNodeValue(elementNode);
-        if ("Integer".equals(type)) {
-          value = Integer.valueOf((String)value);
-        } else if ("Double".equals(type)) {
-          value = Double.valueOf((String)value);
-        } else if ("Long".equals(type)) {
-          value = Long.valueOf((String)value);
-        } /* else {
-          value = value;
-        } */
-      } else if ("children".equals(elementNode.getNodeName())) {
+    for ( int e = 0; e < elementNodes.getLength(); e++ ) {
+      Node elementNode = elementNodes.item( e );
+      if ( "id".equals( elementNode.getNodeName() ) ) {
+        id = XmlUtil.getNodeValue( elementNode );
+      } else if ( "value".equals( elementNode.getNodeName() ) ) {
+        value = XmlUtil.getNodeValue( elementNode );
+      } else if ( "type".equals( elementNode.getNodeName() ) ) {
+        String type = XmlUtil.getNodeValue( elementNode );
+        if ( "Integer".equals( type ) ) {
+          value = Integer.valueOf( (String) value );
+        } else if ( "Double".equals( type ) ) {
+          value = Double.valueOf( (String) value );
+        } else if ( "Long".equals( type ) ) {
+          value = Long.valueOf( (String) value );
+        } /*
+           * else { value = value; }
+           */
+      } else if ( "children".equals( elementNode.getNodeName() ) ) {
         NodeList childNodes = elementNode.getChildNodes();
-        for (int c=0;c<childNodes.getLength();c++) {
-          Node childNode = childNodes.item(c);
-          if (childNode.getNodeName().equals("child")) {
+        for ( int c = 0; c < childNodes.getLength(); c++ ) {
+          Node childNode = childNodes.item( c );
+          if ( childNode.getNodeName().equals( "child" ) ) {
             XmlMetaStoreAttribute childElement = new XmlMetaStoreAttribute();
-            childElement.loadAttribute(childNode);
-            addChild(childElement);
+            childElement.loadAttribute( childNode );
+            addChild( childElement );
           }
         }
       }
@@ -100,11 +101,11 @@ public class XmlMetaStoreAttribute implements IMetaStoreAttribute {
   }
 
   @Override
-  public void deleteChild(String entityId) {
+  public void deleteChild( String entityId ) {
     Iterator<IMetaStoreAttribute> it = children.values().iterator();
-    while (it.hasNext()) {
-      IMetaStoreAttribute element= it.next();
-      if (element.getId().equals(entityId)) {
+    while ( it.hasNext() ) {
+      IMetaStoreAttribute element = it.next();
+      if ( element.getId().equals( entityId ) ) {
         it.remove();
         return;
       }
@@ -122,7 +123,7 @@ public class XmlMetaStoreAttribute implements IMetaStoreAttribute {
    * @param id
    *          the id to set
    */
-  public void setId(String id) {
+  public void setId( String id ) {
     this.id = id;
   }
 
@@ -137,7 +138,7 @@ public class XmlMetaStoreAttribute implements IMetaStoreAttribute {
    * @param value
    *          the value to set
    */
-  public void setValue(Object value) {
+  public void setValue( Object value ) {
     this.value = value;
   }
 
@@ -145,34 +146,34 @@ public class XmlMetaStoreAttribute implements IMetaStoreAttribute {
    * @return the children
    */
   public List<IMetaStoreAttribute> getChildren() {
-    return new ArrayList<IMetaStoreAttribute>(children.values());
+    return new ArrayList<IMetaStoreAttribute>( children.values() );
   }
 
   /**
    * @param children
    *          the children to set
    */
-  public void setChildren(List<IMetaStoreAttribute> children) {
+  public void setChildren( List<IMetaStoreAttribute> children ) {
     this.children.clear();
-    for (IMetaStoreAttribute child : children) {
-      this.children.put(child.getId(), child);
+    for ( IMetaStoreAttribute child : children ) {
+      this.children.put( child.getId(), child );
     }
   }
 
-  public void addChild(IMetaStoreAttribute element) {
-    children.put(element.getId(), element);
+  public void addChild( IMetaStoreAttribute element ) {
+    children.put( element.getId(), element );
   }
 
   @Override
   public void clearChildren() {
     children.clear();
   }
-  
+
   @Override
-  public IMetaStoreAttribute getChild(String id) {
-    return children.get(id);
+  public IMetaStoreAttribute getChild( String id ) {
+    return children.get( id );
   }
-  
+
   /**
    * @return the filename
    */
@@ -181,47 +182,60 @@ public class XmlMetaStoreAttribute implements IMetaStoreAttribute {
   }
 
   /**
-   * @param filename the filename to set
+   * @param filename
+   *          the filename to set
    */
-  public void setFilename(String filename) {
+  public void setFilename( String filename ) {
     this.filename = filename;
   }
 
-  protected void appendAttribute(IMetaStoreAttribute attribute, Document doc, Element parentElement) throws MetaStoreException {
-    if (attribute.getId()==null) {
-      throw new MetaStoreException("An attribute has to be non-null");
+  protected void appendAttribute( IMetaStoreAttribute attribute, Document doc, Element parentElement )
+    throws MetaStoreException {
+    if ( attribute.getId() == null ) {
+      throw new MetaStoreException( "An attribute has to be non-null" );
     }
-    Element idElement = doc.createElement("id");
-    idElement.appendChild(doc.createTextNode(attribute.getId()));
-    parentElement.appendChild(idElement);
+    Element idElement = doc.createElement( "id" );
+    idElement.appendChild( doc.createTextNode( attribute.getId() ) );
+    parentElement.appendChild( idElement );
 
-    Element valueElement = doc.createElement("value");
-    valueElement.appendChild(doc.createTextNode(attribute.getValue()!=null ? attribute.getValue().toString() : ""));
-    parentElement.appendChild(valueElement);
-    
-    Element typeElement = doc.createElement("type");
-    typeElement.appendChild(doc.createTextNode(getType(attribute.getValue())));
-    parentElement.appendChild(typeElement);
-    
-    if (!attribute.getChildren().isEmpty()) {
-      Element childrenElement = doc.createElement("children");
-      parentElement.appendChild(childrenElement);
-      for (IMetaStoreAttribute childElement : attribute.getChildren()) {
-        Element child = doc.createElement("child");
-        childrenElement.appendChild(child);
-        appendAttribute(childElement, doc, child);
+    Element valueElement = doc.createElement( "value" );
+    valueElement
+        .appendChild( doc.createTextNode( attribute.getValue() != null ? attribute.getValue().toString() : "" ) );
+    parentElement.appendChild( valueElement );
+
+    Element typeElement = doc.createElement( "type" );
+    typeElement.appendChild( doc.createTextNode( getType( attribute.getValue() ) ) );
+    parentElement.appendChild( typeElement );
+
+    if ( !attribute.getChildren().isEmpty() ) {
+      Element childrenElement = doc.createElement( "children" );
+      parentElement.appendChild( childrenElement );
+      for ( IMetaStoreAttribute childElement : attribute.getChildren() ) {
+        Element child = doc.createElement( "child" );
+        childrenElement.appendChild( child );
+        appendAttribute( childElement, doc, child );
       }
     }
   }
 
-  protected String getType(Object object) {
-    
-    if (object==null) return "String";
-    if (object instanceof String) return "String";
-    if (object instanceof Integer) return "Integer";
-    if (object instanceof Long) return "Long";
-    if (object instanceof Double) return "Double";
-    
+  protected String getType( Object object ) {
+
+    if ( object == null ) {
+      return "String";
+    }
+    if ( object instanceof String ) {
+      return "String";
+    }
+    if ( object instanceof Integer ) {
+      return "Integer";
+    }
+    if ( object instanceof Long ) {
+      return "Long";
+    }
+    if ( object instanceof Double ) {
+      return "Double";
+    }
+
     return "String";
   }
 
--- src/org/pentaho/metastore/stores/xml/XmlMetaStoreElementType.java
+++ src/org/pentaho/metastore/stores/xml/XmlMetaStoreElementType.java
@@ -1,19 +1,19 @@
 /*!
-* This program is free software; you can redistribute it and/or modify it under the
-* terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
-* Foundation.
-*
-* You should have received a copy of the GNU Lesser General Public License along with this
-* program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
-* or from the Free Software Foundation, Inc.,
-* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
-* without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-* See the GNU Lesser General Public License for more details.
-*
-* Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
-*/
+ * This program is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License, version 2.1 as published by the Free Software
+ * Foundation.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with this
+ * program; if not, you can obtain a copy at http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html
+ * or from the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * Copyright (c) 2002-2013 Pentaho Corporation..  All rights reserved.
+ */
 
 package org.pentaho.metastore.stores.xml;
 
@@ -37,7 +37,7 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class XmlMetaStoreElementType implements IMetaStoreElementType {
-  
+
   public static final String XML_TAG = "data-type";
 
   private String namespace;
@@ -47,128 +47,128 @@ public class XmlMetaStoreElementType implements IMetaStoreElementType {
   private String metaStoreName;
 
   private String filename;
-  
+
   /**
    * @param namespace
    * @param id
    * @param name
    * @param description
    */
-  public XmlMetaStoreElementType(String namespace, String id, String name, String description) {
+  public XmlMetaStoreElementType( String namespace, String id, String name, String description ) {
     this.namespace = namespace;
     this.id = id;
     this.name = name;
     this.description = description;
   }
-  
+
   /**
    * Load an XML meta data store data type from file.
-   * @param namespace the namespace 
-   * @param filename the file to load from
+   * 
+   * @param namespace
+   *          the namespace
+   * @param filename
+   *          the file to load from
    */
-  public XmlMetaStoreElementType(String namespace, String filename) throws MetaStoreException {
+  public XmlMetaStoreElementType( String namespace, String filename ) throws MetaStoreException {
     this.namespace = namespace;
-    
-    File file = new File(filename);
+
+    File file = new File( filename );
     this.id = file.getParentFile().getName();
-    
+
     try {
-    
+
       DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
       DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-      Document document = documentBuilder.parse(file);
+      Document document = documentBuilder.parse( file );
       Element elementTypeElement = document.getDocumentElement();
-      
-      loadElementType(elementTypeElement);
-    } catch(Exception e) {
-      throw new MetaStoreException("Unable to load XML metastore element type from file '"+filename+"'", e);
+
+      loadElementType( elementTypeElement );
+    } catch ( Exception e ) {
+      throw new MetaStoreException( "Unable to load XML metastore element type from file '" + filename + "'", e );
     }
   }
-  
-  protected void loadElementType(Node elementTypeNode) {
+
+  protected void loadElementType( Node elementTypeNode ) {
     NodeList childNodes = elementTypeNode.getChildNodes();
-    for (int e=0;e<childNodes.getLength();e++) {
-      Node childNode = childNodes.item(e);
-      if ("name".equals(childNode.getNodeName())) {
-        name = XmlUtil.getNodeValue(childNode);
+    for ( int e = 0; e < childNodes.getLength(); e++ ) {
+      Node childNode = childNodes.item( e );
+      if ( "name".equals( childNode.getNodeName() ) ) {
+        name = XmlUtil.getNodeValue( childNode );
       }
-      if ("description".equals(childNode.getNodeName())) {
-        description = XmlUtil.getNodeValue(childNode);
+      if ( "description".equals( childNode.getNodeName() ) ) {
+        description = XmlUtil.getNodeValue( childNode );
       }
     }
   }
-  
+
   public void save() throws MetaStoreException {
     FileOutputStream fos = null;
     try {
-      fos = new FileOutputStream(filename);
-      StreamResult result = new StreamResult(fos);
-      saveToStreamResult(result);
-    } catch(Exception e) {
-      throw new MetaStoreException("Unable to save XML meta store data type with file '"+filename+"'", e);
+      fos = new FileOutputStream( filename );
+      StreamResult result = new StreamResult( fos );
+      saveToStreamResult( result );
+    } catch ( Exception e ) {
+      throw new MetaStoreException( "Unable to save XML meta store data type with file '" + filename + "'", e );
     } finally {
-      if (fos!=null) {
+      if ( fos != null ) {
         try {
           fos.close();
-        } catch(Exception e) {
-          throw new MetaStoreException("Unable to save XML meta store data type with file '"+filename+"' (close failed)", e);
+        } catch ( Exception e ) {
+          throw new MetaStoreException( "Unable to save XML meta store data type with file '" + filename
+              + "' (close failed)", e );
         }
       }
     }
   }
-  
+
   public String getXml() throws MetaStoreException {
     try {
       StringWriter stringWriter = new StringWriter();
-      StreamResult result = new StreamResult(stringWriter);
-      saveToStreamResult(result);
-      
+      StreamResult result = new StreamResult( stringWriter );
+      saveToStreamResult( result );
+
       return result.toString();
-    } catch(Exception e) {
-      throw new MetaStoreException("Unable to get XML form of meta store.", e);
+    } catch ( Exception e ) {
+      throw new MetaStoreException( "Unable to get XML form of meta store.", e );
     }
   }
 
-  
-  public void saveToStreamResult(StreamResult streamResult) throws MetaStoreException {
+  public void saveToStreamResult( StreamResult streamResult ) throws MetaStoreException {
     try {
-      
+
       DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
       DocumentBuilder builder = factory.newDocumentBuilder();
       Document doc = builder.newDocument();
-      
-      Element elementTypeElement = doc.createElement(XML_TAG);
-      doc.appendChild(elementTypeElement);
-      
-      appendElementType(doc, elementTypeElement);
-      
+
+      Element elementTypeElement = doc.createElement( XML_TAG );
+      doc.appendChild( elementTypeElement );
+
+      appendElementType( doc, elementTypeElement );
+
       // Write the document content into the data type XML file
       //
       TransformerFactory transformerFactory = TransformerFactory.newInstance();
       Transformer transformer = transformerFactory.newTransformer();
-      transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-      
-      DOMSource source = new DOMSource(doc);
-      
+      transformer.setOutputProperty( "{http://xml.apache.org/xslt}indent-amount", "2" );
+      transformer.setOutputProperty( OutputKeys.INDENT, "yes" );
+
+      DOMSource source = new DOMSource( doc );
+
       // Do the actual saving...
-      transformer.transform(source, streamResult);
-    } catch(Exception e) {
-      throw new MetaStoreException("Unable to serialize XML meta store to stream result", e);
-    } 
+      transformer.transform( source, streamResult );
+    } catch ( Exception e ) {
+      throw new MetaStoreException( "Unable to serialize XML meta store to stream result", e );
+    }
   }
 
+  protected void appendElementType( Document doc, Element elementTypeElement ) {
+    Element nameElement = doc.createElement( "name" );
+    nameElement.appendChild( doc.createTextNode( name ) );
+    elementTypeElement.appendChild( nameElement );
 
-
-
-  protected void appendElementType(Document doc, Element elementTypeElement) {
-    Element nameElement = doc.createElement("name");
-    nameElement.appendChild(doc.createTextNode(name));
-    elementTypeElement.appendChild(nameElement);
-    
-    Element descriptionElement = doc.createElement("description");
-    descriptionElement.appendChild(doc.createTextNode(description));
-    elementTypeElement.appendChild(descriptionElement);
+    Element descriptionElement = doc.createElement( "description" );
+    descriptionElement.appendChild( doc.createTextNode( description ) );
+    elementTypeElement.appendChild( descriptionElement );
 
   }
 
@@ -178,46 +178,57 @@ public class XmlMetaStoreElementType implements IMetaStoreElementType {
   public String getNamespace() {
     return namespace;
   }
+
   /**
-   * @param namespace the namespace to set
+   * @param namespace
+   *          the namespace to set
    */
-  public void setNamespace(String namespace) {
+  public void setNamespace( String namespace ) {
     this.namespace = namespace;
   }
+
   /**
    * @return the id
    */
   public String getId() {
     return id;
   }
+
   /**
-   * @param id the id to set
+   * @param id
+   *          the id to set
    */
-  public void setId(String id) {
+  public void setId( String id ) {
     this.id = id;
   }
+
   /**
    * @return the name
    */
   public String getName() {
     return name;
   }
+
   /**
-   * @param name the name to set
+   * @param name
+   *          the name to set
    */
-  public void setName(String name) {
+  public void setName( String name ) {
     this.name = name;
   }
+
   /**
    * @return the description
    */
   public String getDescription() {
     return description;
   }
+
   /**
-   * @param description the description to set
+   * @param description
+   *          the description to set
    */
-  public void setDescription(String description) {
+  public void setDescription( String description ) {
     this.description = description;
   }
 
@@ -229,9 +240,10 @@ public class XmlMetaStoreElementType implements IMetaStoreElementType {
   }
 
   /**
-   * @param filename the filename to set
+   * @param filename
+   *          the filename to set
    */
-  public void setFilename(String filename) {
+  public void setFilename( String filename ) {
     this.filename = filename;
   }
 
@@ -239,9 +251,8 @@ public class XmlMetaStoreElementType implements IMetaStoreElementType {
     return metaStoreName;
   }
 
-  public void setMetaStoreName(String metaStoreName) {
+  public void setMetaStoreName( String metaStoreName ) {
     this.metaStoreName = metaStoreName;
   }
-
 
 }
