6c9ebc895f844983a5639050b5a62a3d805b3bd7
Kurtis Walker <kwalker@pentaho.com>
2015-04-13 10:10:07 -0400

    [BACKLOG-2971] - ensuring types get registered when they are encountered for the first time.  applying checkstyle rules

--- test-src/org/pentaho/metastore/stores/xml/XmlMetaStoreCacheTestBase.java
+++ test-src/org/pentaho/metastore/stores/xml/XmlMetaStoreCacheTestBase.java
@@ -36,21 +36,21 @@ import org.pentaho.metastore.api.IMetaStoreElementType;
 public abstract class XmlMetaStoreCacheTestBase {
 
   private XmlMetaStoreCache simpleXmlMetaStoreCache;
-  
+
   @Before
   public void before() {
     simpleXmlMetaStoreCache = createMetaStoreCache();
   }
-  
+
   protected abstract XmlMetaStoreCache createMetaStoreCache();
-  
+
   @Test
   public void registerElementTypeIdForName() {
     simpleXmlMetaStoreCache.registerElementTypeIdForName( "testNamespace", "testElementTypeName", "testElementTypeId" );
     String actualElementId = simpleXmlMetaStoreCache.getElementTypeIdByName( "testNamespace", "testElementTypeName" );
     assertEquals( "testElementTypeId", actualElementId );
   }
-  
+
   @Test
   public void unregisterElementTypeIdForName() {
     simpleXmlMetaStoreCache.registerElementTypeIdForName( "testNamespace", "testElementTypeName", "testElementTypeId" );
@@ -58,16 +58,20 @@ public abstract class XmlMetaStoreCacheTestBase {
     String actualElementId = simpleXmlMetaStoreCache.getElementTypeIdByName( "testNamespace", "testElementTypeName" );
     assertNull( actualElementId );
   }
-  
+
   @Test
   public void registerElementIdForName() {
     IMetaStoreElementType testElementType = createTestElementType( "testElementTypeName", "testElementTypeId" );
+    IMetaStoreElementType secondElementType = createTestElementType( "secondElementTypeName", "secondElementTypeId" );
     simpleXmlMetaStoreCache.registerElementTypeIdForName( "testNamespace", testElementType.getName(), testElementType.getId() );
     simpleXmlMetaStoreCache.registerElementIdForName( "testNamespace", testElementType, "testElementName", "testElementId" );
+    simpleXmlMetaStoreCache.registerElementIdForName( "testNamespace", secondElementType, "secondElementName", "secondElementId" );
     String actualElementId = simpleXmlMetaStoreCache.getElementIdByName( "testNamespace", testElementType, "testElementName" );
     assertEquals( "testElementId", actualElementId );
+    String actualSecondElementId = simpleXmlMetaStoreCache.getElementIdByName( "testNamespace", secondElementType, "secondElementName" );
+    assertEquals( "secondElementId", actualSecondElementId );
   }
-  
+
   @Test
   public void registerElementIdForName_for_null_id() {
     IMetaStoreElementType testElementType = createTestElementType( "testElementTypeName", "testElementTypeId" );
@@ -76,7 +80,7 @@ public abstract class XmlMetaStoreCacheTestBase {
     String actualElementId = simpleXmlMetaStoreCache.getElementIdByName( "testNamespace", testElementType, "testElementName" );
     assertNull( actualElementId );
   }
-  
+
   @Test
   public void registerElementIdForName_for_non_registered_type() {
     IMetaStoreElementType testElementType = createTestElementType( "testElementTypeName", "testElementTypeId" );
@@ -84,7 +88,7 @@ public abstract class XmlMetaStoreCacheTestBase {
     String actualElementId = simpleXmlMetaStoreCache.getElementIdByName( "testNamespace", testElementType, "testElementName" );
     assertEquals( "testElementId", actualElementId );
   }
-  
+
   @Test
   public void unregisterElementIdForName() {
     IMetaStoreElementType testElementType = createTestElementType( "testElementTypeName", "testElementTypeId" );
@@ -94,15 +98,15 @@ public abstract class XmlMetaStoreCacheTestBase {
     String actualElementId = simpleXmlMetaStoreCache.getElementIdByName( "testNamespace", testElementType, "testElementName" );
     assertNull( "testElementId", actualElementId );
   }
-  
+
   @Test
   public void registerProcessedFile() {
     simpleXmlMetaStoreCache.registerProcessedFile( "/test/full/Path", 1L );
     Map<String, Long> actualProcessedFiles = simpleXmlMetaStoreCache.getProcessedFiles();
     assertThat( actualProcessedFiles.size(), equalTo( 1 ) );
-    assertThat( actualProcessedFiles.containsKey( "/test/full/Path" ), is( true ));
+    assertThat( actualProcessedFiles.containsKey( "/test/full/Path" ), is( true ) );
   }
-  
+
   @Test
   public void unregisterProcessedFile() {
     simpleXmlMetaStoreCache.registerProcessedFile( "/test/full/Path", 1L );
@@ -110,7 +114,7 @@ public abstract class XmlMetaStoreCacheTestBase {
     Map<String, Long> actualProcessedFiles = simpleXmlMetaStoreCache.getProcessedFiles();
     assertThat( actualProcessedFiles.size(), equalTo( 0 ) );
   }
-  
+
   @Test
   public void clear() {
     simpleXmlMetaStoreCache.registerProcessedFile( "/test/full/Path", 1L );
@@ -123,14 +127,14 @@ public abstract class XmlMetaStoreCacheTestBase {
     String actualElementId = simpleXmlMetaStoreCache.getElementIdByName( "testNamespace", testElementType, "testElementName" );
     assertThat( actualElementId, is( nullValue() ) );
   }
-  
+
   @Test
   public void clear_empty_cache() {
     simpleXmlMetaStoreCache.clear();
     Map<String, Long> actualProcessedFiles = simpleXmlMetaStoreCache.getProcessedFiles();
     assertThat( actualProcessedFiles.size(), equalTo( 0 ) );
   }
-  
+
   private static IMetaStoreElementType createTestElementType( String typeName, String typeId ) {
     IMetaStoreElementType testElementType = mock( IMetaStoreElementType.class );
     when( testElementType.getName() ).thenReturn( typeName );
